3befa01a7c42abf0d2a48421503543187794fdb0
Jinnan Fan <jfan084@uottawa.ca>
2023-05-24 15:29:06 -0400

    refactoring

---
+++ game.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+from tkinter import Frame, Label
+import random
+
+
+class Game:
+    def __init__(self, gameFrame):
+        self.gameOver = False
+        self.score = 0
+        self.create_grid(gameFrame)
+        self.start_game()
+
+    def start_game(self):
+        self.add_new_tile()
+        self.add_new_tile()
+        self.update_gridLabel()
+
+    def create_grid(self, gameFrame):
+        self.gridNumber = [[None for _ in range(4)] for _ in range(4)]
+        self.grid = []
+
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cellFrame = Frame(gameFrame, width=100, height=100)
+                cellFrame.grid(row=i, column=j, padx=5, pady=5)
+                cellLabel = Label(gameFrame, fg="orange", font=("Helvetica", 18))
+                cellLabel.grid(row=i, column=j)
+                row.append({"frame": cellFrame, "label": cellLabel})
+            self.grid.append(row)
+
+        print(self.gridNumber)
+
+    def update_gridLabel(self):
+        for i in range(4):
+            for j in range(4):
+                if self.gridNumber[i][j]:
+                    self.grid[i][j]["label"].configure(text=self.gridNumber[i][j])
+                else:
+                    self.grid[i][j]["label"].configure(text="")
+
+    def update_score(self, score):
+        self.score += score
+
+    def game_over(self):
+        if (
+            not self.emptyCellAvailable()
+            and not self.horizontalMoveAvailable()
+            and not self.verticalMoveAvailable()
+        ):
+            self.gameOver = True
+
+    def horizontalMoveAvailable(self):
+        for i in range(3):
+            for j in range(4):
+                if self.gridNumber[i][j] == self.gridNumber[i + 1][j]:
+                    return True
+        return False
+
+    def verticalMoveAvailable(self):
+        for i in range(4):
+            for j in range(3):
+                if self.gridNumber[i][j] == self.gridNumber[i][j + 1]:
+                    return True
+        return False
+
+    def add_new_tile(self):
+        if self.emptyCellAvailable():
+            while True:
+                row = random.randint(0, 3)
+                col = random.randint(0, 3)
+                if not self.gridNumber[row][col]:
+                    break
+
+            if random.randint(0, 9) == 0:
+                self.gridNumber[row][col] = 4
+            else:
+                self.gridNumber[row][col] = 2
+
+            print("add new tile: ", self.gridNumber)
+
+    def emptyCellAvailable(self):
+        for i in range(4):
+            for j in range(4):
+                if not self.gridNumber[i][j]:
+                    return True
+        return False
+
+    def up(self, event):
+        self.move_tile_up_and_merge()
+        self.after_move()
+
+    def left(self, event):
+        self.transpose_left()
+        self.move_tile_up_and_merge()
+        self.transpose_left()
+        self.after_move()
+
+    def down(self, event):
+        self.upside_down()
+        self.move_tile_up_and_merge()
+        self.upside_down()
+        self.after_move()
+
+    def right(self, event):
+        self.transpose_left()
+        self.upside_down()
+        self.move_tile_up_and_merge()
+        self.upside_down()
+        self.transpose_left()
+        self.after_move()
+
+    def after_move(self):
+        self.add_new_tile()
+        self.update_gridLabel()
+        self.game_over()
+
+    def move_tile_up(self):
+        for i in range(1, 4):
+            for j in range(4):
+                if self.gridNumber[i][j]:
+                    k = i
+                    while not self.gridNumber[k - 1][j] and k > 0:
+                        k -= 1
+                    if k != i:
+                        self.gridNumber[k][j] = self.gridNumber[i][j]
+                        self.gridNumber[i][j] = None
+
+        print("move up: ", self.gridNumber)
+
+    def move_tile_up_and_merge(self):
+        self.move_tile_up()
+        self.merge_tile_up()
+        self.move_tile_up()
+
+    def transpose_left(self):
+        for i in range(4):
+            for j in range(i, 4):
+                self.gridNumber[i][j], self.gridNumber[j][i] = (
+                    self.gridNumber[j][i],
+                    self.gridNumber[i][j],
+                )
+
+        print("transpose_left: ", self.gridNumber)
+
+    def upside_down(self):
+        for i in range(2):
+            for j in range(4):
+                self.gridNumber[i][j], self.gridNumber[3 - i][j] = (
+                    self.gridNumber[3 - i][j],
+                    self.gridNumber[i][j],
+                )
+
+    def merge_tile_up(self):
+        score = 0
+        for i in range(3):
+            for j in range(4):
+                if (
+                    self.gridNumber[i][j]
+                    and self.gridNumber[i][j] == self.gridNumber[i + 1][j]
+                ):
+                    self.gridNumber[i][j] *= 2
+                    self.gridNumber[i + 1][j] = None
+                    score += self.gridNumber[i][j]
+
+        self.update_score(score)
+        print("merge up: ", self.gridNumber)
+
+    def __repr__(self):
+        return self.gridNumber
---
+++ main.py
@@ -0,0 +1,21 @@
+from game import Game
+from tkinter import *
+
+root = Tk()
+root.configure(bg='black')
+root.title("2048")
+
+scoreBoard = Frame(root, bg='green', width=100, height=100)
+scoreBoard.grid(row=0,column=0, columnspan=4)
+
+gameBoard = Frame(root, bg='red', width=400, height=400)
+gameBoard.grid(row=1,column=0)
+
+game = Game(gameBoard)
+
+root.bind("<Up>", game.up)
+root.bind("<Down>", game.down)
+root.bind("<Left>", game.left)
+root.bind("<Right>", game.right)
+
+root.mainloop()           
